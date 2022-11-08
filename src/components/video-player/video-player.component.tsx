d08977d89109e5211c3b50b157bfbc0b2f4c63eb
Eshau <david021484@gmail.com>
2022-11-08 22:27:20 +0800

    [解決]直播未持續播放，解決方法：server on_publish 及 on_publish_done 未正確回傳 response

--- src/components/video-player/video-player.component.tsx
+++ src/components/video-player/video-player.component.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import videojs from "video.js";
 import VideoJs from "@/components/video-js/video-js.component";
 
-import { Container } from "./video-player.style"
+import Hls from "hls.js";
+import HlsJs from "@/components/hls-js/hls-js.component";
+
+import { Container, Video } from "./video-player.style";
 
 interface IVideoPlayerProps {
   isStreamOn: boolean;
 }
 
-const VideoPlayer: React.FC<IVideoPlayerProps> = ({ isStreamOn }) => {
+const VideoJsPlayer: React.FC<IVideoPlayerProps> = ({ isStreamOn }) => {
   const playerRef = useRef<null | videojs.Player>(null);
 
   const videoJsOptions = {
@@ -46,4 +49,36 @@ const VideoPlayer: React.FC<IVideoPlayerProps> = ({ isStreamOn }) => {
   );
 };
 
-export default VideoPlayer;
+const HlsJsPlayer: React.FC<IVideoPlayerProps> = ({ isStreamOn }) => {
+  const videoRef = useRef(null);
+
+  // useEffect(() => {
+  //   fetch("http://192.168.64.1:3000/check-stream")
+  //     .then((res) => res.json())
+  //     .then((result) => setIsStreamOn(result))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  useEffect(() => {
+    if (!videoRef.current || !isStreamOn) return;
+
+    if (Hls.isSupported()) {
+      const video = videoRef.current;
+      const hls = new Hls();
+      hls.attachMedia(video);
+
+      hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+        hls.loadSource(
+          "http://192.168.64.2/3/index.m3u8"
+        );
+      });
+    }
+  }, [videoRef, isStreamOn]);
+  return (
+    <Container>
+      <Video controls ref={videoRef} id="video" />
+    </Container>
+  );
+};
+
+export default HlsJsPlayer;
