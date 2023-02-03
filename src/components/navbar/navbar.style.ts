5fdc9916e4a00347c778a02d4d44153b5df3e6cb
Eshau <david021484@gmail.com>
2023-02-03 14:11:22 +0800

    [新增]1.從server抓取影片2.點擊連結至指定影片

---
+++ src/components/navbar/navbar.style.ts
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: #0393af;
+`;
+
+export const LinkList = styled.div`
+  display: flex;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  padding: 1.8rem;
+  color: #2b333fbf;
+  font-weight: bold;
+  transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #2b333fbf;
+    color: white;
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  }
+`;
