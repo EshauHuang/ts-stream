99637e21735c0fa092289dceed2a3698a93a4beb
Eshau <david021484@gmail.com>
2023-03-14 10:24:03 +0800

    [更新]將 backend 及frontend 分開

--- frontend/public/nginx/hls-segment.sh
+++ frontend/public/nginx/hls-segment.sh
@@ -7,7 +7,7 @@ echo "$(date) - Starting HLS segment generation for stream $name" >> /tmp/log/hl
 mkdir -p /tmp/records/$name/
 
 # Generate the HLS segments using ffmpeg
-ffmpeg -i rtmp://localhost:1935/hls_live/$name -c:a aac -ac 2 -b:a 128k -c:v libx264 -preset medium -profile:v baseline -sc_threshold 0 -hls_time 10 -hls_list_size 0 -f hls -hls_segment_filename "/tmp/records/$name/%d.ts" /tmp/records/$name/index.m3u8
+/usr/local/bin/ffmpeg -i rtmp://localhost:1935/hls_live/$name -c:a aac -ac 2 -b:a 128k -c:v libx264 -preset medium -profile:v baseline -sc_threshold 0 -hls_time 10 -hls_list_size 0 -f hls -hls_segment_filename "/tmp/records/$name/%d.ts" /tmp/records/$name/index.m3u8
 
 echo "$(date) - Finished HLS segment generation for stream $name" >> /tmp/log/hls-segment.log
 
