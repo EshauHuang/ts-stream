024d6c22b257c226cb7c5759f55b299951c44d62
Eshau <david021484@gmail.com>
2023-05-06 15:24:34 +0800

    [新增] nginx-reverse-proxy

--- backend/index.js
+++ backend/index.js
@@ -48,6 +48,12 @@ export const io = new Server(server, {
 
 startIo(io);
 
+// app.use("/", signRoute);
+// app.use("/users", usersRoute);
+// app.use("/streams", streamsRoute);
+// app.use("/videos", videosRoute);
+// app.use("/live", liveRoute);
+
 apiRouter.use("/", signRoute);
 apiRouter.use("/users", usersRoute);
 apiRouter.use("/streams", streamsRoute);
